e6357d10ad64e09a9caeced5d158a6c39cda528f
Yuta Katayama <yutapilafu@gmail.com>
2021-04-10 13:42:07 +0900

    404ページとheaderの追加

--- src/app/feature/feature.module.ts
+++ src/app/feature/feature.module.ts
@@ -5,15 +5,23 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
 
 import { environment } from '../../environments/environment';
+import { HeaderComponent } from './components/header/header.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NotFoundComponent
+  ],
   imports: [
     CommonModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
     AngularFireDatabaseModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class FeatureModule { }
