f55bb20261d6ea3be43ed9ca67edc318711143b1
Yuta Katayama <yutapilafu@gmail.com>
2021-04-10 17:13:05 +0900

    リダイレクトの設定を追加

--- src/app/feature/services/auth.service.ts
+++ src/app/feature/services/auth.service.ts
@@ -12,6 +12,13 @@ export class AuthService {
   // Namespace '"C:/Users/user/OneDrive/\u30C9\u30AD\u30E5\u30E1\u30F3\u30C8/chat-talk/node_modules/firebase/index"'
   // has no exported member 'auth'.
   create(email: string, password: string): Promise<any> {
-    return this.auth.createUserWithEmailAndPassword(email, password);
+    return this.auth.createUserWithEmailAndPassword(email, password)
+      .then(credential => {
+        const { user } = credential;
+        const actionCodeSettings = {
+          url: `http://localhost:4200/?newAccount=true&email=${user.email}`
+        };
+        user.sendEmailVerification(actionCodeSettings);
+      });
   }
 }
